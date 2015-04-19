3e65c6551320081e34db0b596b7d2ba9df9d5d9a
Anthony <anthony@aecore.com>
2015-04-19 09:14:17 -0700

    models located in app/models/xyz for organization. Updated composer.json autoload app\models

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,4 @@
-<?php namespace App;
+<?php namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
@@ -22,8 +22,9 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
-
+	protected $fillable = ['name', 'email', 'username', 'password', 'token'];
+  // token = random string (10) unique identifier used throughout the site to prevent single digit user id entries.
+  
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
