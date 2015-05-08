3dd08d28e9b11a4a69154c967ab3e2ca65c14374
karthickkumar-aj <karthickkumar.aj@gmail.com>
2015-05-08 19:12:51 +0000

    Added controller and view for projects

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">Aecore :: Web Based Construction Project Management Tool</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
